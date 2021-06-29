52fccdb9f9eefe1f41c7a73de01bee25398ad916
Heider Sanchez <heider.sanchez.pe@gmail.com>
2021-06-28 19:44:17 -0500

    fix main

--- main.cpp
+++ main.cpp
@@ -12,21 +12,21 @@ vector<pair<string, string>> loadCSV(string file);
 
 int main()
 {
-  vector<pair<string, string>> data = loadCSV("data_small.csv");
-  unordered_map<string, string> map;
+  vector<pair<string, string>> data = loadCSV("smalldata.csv");
+  unordered_map<string, string> hash; //reeamplazar por su propia implementacion de hash
 
   auto start = high_resolution_clock::now();
   for (auto par : data)
-    map[par.first] = par.second;
+    hash[par.first] = par.second;  
   auto stop = high_resolution_clock::now();
   auto duration = duration_cast<microseconds>(stop - start);
   cout << "Time:" << duration.count() << endl;
-  cout << "Size of Hash Table: " << map.bucket_count() << endl;
+  cout << "Size of Hash Table: " << hash.bucket_count() << endl;
 
-  for (unsigned i = 0; i < map.bucket_count(); ++i)
+  for (unsigned i = 0; i < hash.bucket_count(); ++i)
   {
-    cout << "bucket #" << i << " contains " << map.bucket_size(i) << " elements: ";
-    for (auto it = map.begin(i); it != map.end(i); ++it)
+    cout << "bucket #" << i << " contains " << hash.bucket_size(i) << " elements: ";
+    for (auto it = hash.begin(i); it != hash.end(i); ++it)
       cout << "[" << it->first << ":" << it->second << "] ";
     cout << "\n";
   }
